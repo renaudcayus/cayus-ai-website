ded3aec93c58adc8f365b1184abed007343bc897
Umar Saleem <umersaleem50@gmail.com>
2025-11-17 23:00:46 +0500

    fix: Transparent grid always on top of page fixed.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = generateMetadata();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="scrollbar">
+    <html lang="en" className="scrollbar relative">
       <body
         className={cn(
           'min-h-screen bg-background text-foreground antialiased !font-default overflow-x-hidden',
